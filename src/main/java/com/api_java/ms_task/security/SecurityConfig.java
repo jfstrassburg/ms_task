70b783988c9dd623a735f794da66b135db5ac3c4
Juliano <1415341@sga.pucminas.br>
2025-05-05 17:10:12 -0300

    ajuste configuração keycloak

--- src/main/java/com/api_java/ms_task/security/SecurityConfig.java
+++ src/main/java/com/api_java/ms_task/security/SecurityConfig.java
@@ -1,66 +1,30 @@
 package com.api_java.ms_task.security;
 
 
-import lombok.RequiredArgsConstructor;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
-
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
-@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
-
 public class SecurityConfig {
-
-
+    
+    // ROLE_USER
+    // ROLE_ADMIN
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http.cors().and().csrf().disable();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http ) throws Exception {
+
+        http
+        .csrf(csrf -> csrf.disable())
+        .oauth2ResourceServer(oauth2 -> oauth2
+        .jwt(jwt -> jwt.jwtAuthenticationConverter(new JWTConverter())));
 
 
-        http.authorizeHttpRequests()
-                .requestMatchers("/cadastroController/**").permitAll()
-                .requestMatchers("/**").permitAll()
-                .requestMatchers("/api/tarefas/**").permitAll()
-                .anyRequest().authenticated();
-
-
-
-        http.oauth2ResourceServer()
-                .jwt()
-                .jwtAuthenticationConverter(this.jwtAuthenticationConverter());
-
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
     }
-
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//        KeycloakAuthenticationProvider keycloakAuthenticationProvider = new KeycloakAuthenticationProvider();
-//        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
-//        auth.authenticationProvider(keycloakAuthenticationProvider);
-//    }
-
-
-    private Converter<Jwt, ? extends AbstractAuthenticationToken> jwtAuthenticationConverter() {
-        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
-        converter.setJwtGrantedAuthoritiesConverter(new RealmRoleConverter());
-        return converter;
-    }
-
-
-
 }
